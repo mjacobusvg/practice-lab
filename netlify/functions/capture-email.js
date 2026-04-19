c000ec1bb5e64b723a026068b8bfe029c2668d40
mjacobusvg <michael@thinkbeyondpsych.com>
2026-04-19 11:16:13 -0700

    Update capture-email.js

--- netlify/functions/capture-email.js
+++ netlify/functions/capture-email.js
@@ -105,7 +105,7 @@ exports.handler = async (event, context) => {
         const circleResponse = await fetch('https://app.circle.so/api/v1/community_members', {
           method: 'POST',
           headers: {
-            'Authorization': `Token ${process.env.CIRCLE_API_TOKEN}`,
+            'Authorization': `Token VNpKR2mr2AMqvrCEvoK9Kewr4FE12GaA`,
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
